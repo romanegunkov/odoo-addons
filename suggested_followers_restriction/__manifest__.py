95d103001c9bd2defbb72ae99b7167e15c5d1cc1
Roman Egunkov <romanegunkov@gmail.com>
2018-11-02 03:30:24 +0300

    Comment views, because not preset yet.

--- suggested_followers_restriction/__manifest__.py
+++ suggested_followers_restriction/__manifest__.py
@@ -38,8 +38,8 @@
 
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        # 'views/views.xml',
+        # 'views/templates.xml',
     ],    
     'demo': [],
     'qweb': [
